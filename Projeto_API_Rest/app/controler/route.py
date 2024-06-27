c4f07ea232bf7f558876c6f432424c4f9c7bedd8
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2024-06-27 17:24:17 -0300

    Finalizando CRUD com Pythhon e Flask e API rest com Banco de dados SQLite. Aplicação com todas as funções e rotas definidas para buscar todos os ususario do banco, buscar apenas um, adicionar ususario, atualizar e deletar.

--- Projeto_API_Rest/app/controler/route.py
+++ Projeto_API_Rest/app/controler/route.py
@@ -31,26 +31,90 @@ def index():
     return render_template('usuario.html')
 
 
-##-----CRUD------#
-#Selecionar todo
-#Selecionar um
-#Cadastrar
-#Atualizar
-#Deletar
+##-------------------------------CRUD------------------------------------#
+
+
+
+#==========Selecionar todo=============#
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):# função criada mostrar qual o conteudo do banco e uuma mensagem se necessario
+    body = {}
+    body[nome_do_conteudo] = conteudo
+    if(mensagem):
+        body["mensagem"]= mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="aplication/json")
 
 @app.route('/select', methods=['GET'])
 def select():
-    select_user = User.query.all()# retorna um objeto (uma lista)
-    usuario_json = [usuario.to_json() for usuario in select_user]# criando uma lista e aplicando o metodo 'to_json' em todos da lista
+    select_all = User.query.all()# retorna um objeto (uma lista)
+    usuario_json = [usuario.to_json() for usuario in select_all]# criando uma lista e aplicando o metodo 'to_json' em todos da lista
     print(usuario_json)
-    return Response(json.dumps(usuario_json))#A função json.dumps() converterá um subconjunto de objetos Python em uma string json. Nem todos os objetos são conversíveis e pode ser necessário criar um dicionário de dados que deseja expor antes de serializar para JSON.
+    #return Response(json.dumps(usuario_json)) A função json.dumps() converterá um subconjunto de objetos Python em uma string json. Nem todos os objetos são conversíveis e pode ser necessário criar um dicionário de dados que deseja expor antes de serializar para JSON.
+    return gera_response(200,"usuarios", usuario_json, "ok")
  
 
-@app.route('/add', methods=['GET','POST'])
-def add_user():
-    if request.method == 'POST':
-        usuario = User(request.form['name'], request.form['password'])
+#===============Selecionar um====================#
+
+@app.route("/selectuser/<id>", methods=["GET"])
+def selec_user(id):
+    select_user = User.query.filter_by(id=id).first()#filter_by(id=id) é para filtrar apenas um ususario o banco o .first() e para retornar apenas um objeto(com a busca e por ID não há problema porque e primari key e so existe um)
+    usuario_json = select_user.to_json()
+    print(usuario_json)
+    return gera_response(200, "usuario", usuario_json)
+    
+
+
+
+#=================Cadastrar======================#
+
+@app.route("/add", methods=["POST"])
+def cadastro_user():
+    #validar se recebeu os parametros
+    #ou utilizar try catch para gerar erro
+
+    body = request.get_json()
+
+    try:
+        usuario = User(name=body["name"], password=body["password"])
         db.session.add(usuario)
         db.session.commit()
-        return redirect(url_for('index'))
-    return 'Erro'
+        return gera_response(201, "usuario", usuario.to_json(), "Criado com sucesso")
+    except Exception as e:
+        print("Erro",e)
+        return gera_response(400, "", {}, "Erro ao cadastrar")
+
+
+#=================Atualizar======================#
+@app.route("/atualiza/<id>", methods=["PUT"])
+def atualiza_user(id):
+    #selecioana usuario
+    #seleciona modificações
+    
+    selec_user = User.query.filter_by(id=id).first()
+    body = request.get_json()
+
+    try:
+        if('name'  in body):
+            selec_user.name = body['name']
+        if('password' in body):
+            selec_user.password = body['password']
+
+            db.session.add(selec_user)
+            db.session.commit()
+            return gera_response(200, "usuario", selec_user.to_json(), "Atualizado com Sucesso")
+    except Exception as e:
+        return gera_response(400, "usuario", {}, "Erro ao atualizar")
+
+
+#==================Deletar=======================#
+@app.route('/delete/<id>',methods=["DELETE"])
+def delet_user(id):
+    selec_user = User.query.filter_by(id=id).first()
+
+    try:
+        db.session.delete(selec_user)
+        db.session.commit()
+        return gera_response(200, "usuario", selec_user.to_json(),"Deletado com sucesso")
+    except Exception as e:
+        return gera_response(400,"usuario",{}, "Erro ao deletar")
